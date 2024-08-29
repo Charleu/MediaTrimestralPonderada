2cd0582c6dd7fe66d16ff76a4bc54eef84697a75
Charleu <sdlimeira0@gmail.com>
2024-08-29 12:13:56 -0300

    primeiro commit

---
+++ MediaTrimestralPonderada/Aluno.cs
@@ -0,0 +1,47 @@
+﻿
+namespace MediaTrimestralPonderada
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public Aluno(string nome, double nota1, double nota2, double nota3)
+        {
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+
+        public double CalculaNotaFinal()
+        {
+            return (Nota1 * 30 + Nota2 * 35 + Nota3 * 35) / 100;
+        }
+
+        public void ExibirResultado()
+        {
+            string separador = new string('-', 50);
+            double notaFinal = CalculaNotaFinal();
+            string resultado;
+
+            if (notaFinal >= 60)
+            {
+                resultado = "Aluno aprovado!";
+            }
+            else
+            {
+                double pontosFaltantes = 60 - notaFinal;
+                resultado = $"Aluno reprovado!Pontos restantes para ser aprovado: {pontosFaltantes.ToString("F2")}";
+            }
+
+            Console.WriteLine(separador);
+            Console.WriteLine($"Nota final de {Nome}: {notaFinal.ToString("F2")}");
+            Console.WriteLine(separador);
+            Console.WriteLine(resultado);
+            Console.WriteLine(separador);
+        }
+    }
+}
---
+++ MediaTrimestralPonderada/Program.cs
@@ -0,0 +1,23 @@
+﻿using MediaTrimestralPonderada;
+using System;
+
+namespace MediaTrimestralPoderada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do aluno:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe a nota do primeiro semestre:");
+            double nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota do segundo semestre:");
+            double nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota do terceiro semestre:");
+            double nota3 = double.Parse(Console.ReadLine());
+
+            Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+            aluno.ExibirResultado();
+        }
+    }
+}
