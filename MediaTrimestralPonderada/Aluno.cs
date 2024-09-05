20e4149c8c258269b1b1cae196696f2e8c548d78
Charleu <sdlimeira0@gmail.com>
2024-09-05 17:37:02 -0300

    simplificando as propriedades get e set completas na classe Aluno

--- MediaTrimestralPonderada/Aluno.cs
+++ MediaTrimestralPonderada/Aluno.cs
@@ -3,34 +3,10 @@ namespace MediaTrimestralPonderada
 {
     class Aluno
     {
-        private string _nome;
-        private double _nota1;
-        private double _nota2;
-        private double _nota3;
-
-        public string Nome
-        {
-            get { return _nome; }
-            set { _nome = value; }
-        }
-
-        public double Nota1
-        {
-            get { return _nota1; }
-            set { _nota1 = value; }
-        }
-
-        public double Nota2
-        {
-            get { return _nota2; }
-            set { _nota2 = value; }
-        }
-
-        public double Nota3
-        {
-            get { return _nota3; }
-            set { _nota3 = value; }
-        }
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
 
         public Aluno(string nome, double nota1, double nota2, double nota3)
         {
